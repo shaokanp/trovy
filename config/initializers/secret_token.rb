a8ab09daf83f721f26b17735509f98597c96dae5
shaokanp <whobirdsyou@gmail.com>
2013-07-11 00:00:47 +0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trovy::Application.config.secret_key_base = '2962d686ec02c051bad56d087a4f8bb43f7dacba62a2b800dd276714c778f25eafaf61cd822d25fbfbae18d10c5e264a295c7b3d58caddcb395f14b7860d1d95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trovy::Application.config.session_store :cookie_store, key: '_trovy_session'
